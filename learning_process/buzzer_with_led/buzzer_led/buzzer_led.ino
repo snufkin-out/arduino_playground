eef9001d89af40e4d0f649a7a0acc5fba3105599
snufkin-out <nicolas.marte@hotmail.com>
2024-08-10 11:48:34 +0200

    add fix counter strike bomb bug

--- learning_process/buzzer_with_led/buzzer_led/buzzer_led.ino
+++ learning_process/buzzer_with_led/buzzer_led/buzzer_led.ino
@@ -27,19 +27,18 @@ void bomb() {
     long elapsedTime = currentTime - startTime;
 
     if (elapsedTime <= 45000) { // until 45 seconds
-        remainingTime = 45 - (elapsedTime / 1000); // Calculate the remaining time in seconds
-        int delayTime = map(elapsedTime, 0, 45000, 1000, 50);
-
-        tone(buzzerPin, 500);  // buzzer on for a constant duration
-        delay(150);            // beep duration stays the same
-        noTone(buzzerPin);
-        delay(delayTime);      // delay between beeps decreases over time
+      remainingTime = 45 - (elapsedTime / 1000); // Calculate the remaining time in seconds
+      int delayTime = map(elapsedTime, 0, 45000, 1000, 50);  // Maps the elapsed time to a delay range of 1000ms to 100ms
+      tone(buzzerPin, 500);
+      delay(150);
+      noTone(buzzerPin);
+      delay(delayTime);
     } else {
         remainingTime = 0; // timer finished
 
         // beep 8 times after 45 seconds to simulate "explosion"
         for (int i = 0; i < 8; i++) {
-            tone(buzzerPin, 500);
+            tone(buzzerPin, 1000);
             delay(50);
             noTone(buzzerPin);
             delay(50);
